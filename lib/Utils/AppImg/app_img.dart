854aa5013cac7138e629be4f6db2d8a11bda6947
srsakilhossen302 <cse.sakilhossem@gmail.com>
2026-04-11 01:10:36 +0600

    feat(login/signup): add theme toggle to auth screens
    Add dark/light mode toggle button to the app bar of login and signup screens.
    Also add a new image asset reference for terms agreement.

--- lib/Utils/AppImg/app_img.dart
+++ lib/Utils/AppImg/app_img.dart
@@ -4,4 +4,5 @@ class AppImg {
   static const String onboarding1 = "$basePath/Welcome to Sendit.png";
   static const String onboarding2 = "$basePath/Ship With Confidence.png";
   static const String onboarding3 = "$basePath/Turn Your Trips Into Earnings.png";
+  static const String agreeTerms = "$basePath/Agree to Terms.png";
 }
