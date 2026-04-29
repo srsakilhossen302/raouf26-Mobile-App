b04f1656c58a288f58d4a00320eda34833f7763e
srsakilhossen302 <cse.sakilhossem@gmail.com>
2026-04-30 01:07:43 +0600

    feat: add traveler search view with route selection and navigation integration

--- lib/View/Screen/Traveler-Roll/Search/search_view.dart
+++ lib/View/Screen/Traveler-Roll/Search/search_view.dart
@@ -13,6 +13,8 @@ import '../../../../Utils/AppImg/app_img.dart';
 import 'search_controller.dart';
 import 'map_picker_screen.dart';
 import '../PackageDetails/package_details_view.dart';
+import '../Profile/camera_scan_screen.dart';
+import '../../Transporter-Roll/Notifications/view/transporter_notifications_view.dart';
 
 class SearchScreen extends GetView<TravelerSearchController> {
   const SearchScreen({super.key});
@@ -105,29 +107,35 @@ class SearchScreen extends GetView<TravelerSearchController> {
                               ),
                               Row(
                                 children: [
-                                  Container(
-                                    padding: EdgeInsets.all(10.r),
-                                    decoration: BoxDecoration(
-                                      color: Colors.white.withOpacity(0.2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: Icon(
-                                      Icons.qr_code_scanner,
-                                      color: Colors.white,
-                                      size: 24.sp,
+                                  GestureDetector(
+                                    onTap: () => Get.to(() => const CameraScanScreen()),
+                                    child: Container(
+                                      padding: EdgeInsets.all(10.r),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white.withOpacity(0.2),
+                                        shape: BoxShape.circle,
+                                      ),
+                                      child: Icon(
+                                        Icons.qr_code_scanner,
+                                        color: Colors.white,
+                                        size: 24.sp,
+                                      ),
                                     ),
                                   ),
                                   SizedBox(width: 12.w),
-                                  Container(
-                                    padding: EdgeInsets.all(10.r),
-                                    decoration: BoxDecoration(
-                                      color: Colors.white.withOpacity(0.2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: Icon(
-                                      Icons.notifications_none,
-                                      color: Colors.white,
-                                      size: 24.sp,
+                                  GestureDetector(
+                                    onTap: () => Get.to(() => const TransporterNotificationsView()),
+                                    child: Container(
+                                      padding: EdgeInsets.all(10.r),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white.withOpacity(0.2),
+                                        shape: BoxShape.circle,
+                                      ),
+                                      child: Icon(
+                                        Icons.notifications_none,
+                                        color: Colors.white,
+                                        size: 24.sp,
+                                      ),
                                     ),
                                   ),
                                 ],
